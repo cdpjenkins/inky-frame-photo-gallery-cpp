7b56524a1d26b13c41444a408cd26c811907ed11
Chris Jenkins <cdpjenkins@gmail.com>
2023-01-31 19:24:41 +0000

    Talk to Pico over serial over USB
    I'm not quite sure whether this counts as UART or not

--- main.cpp
+++ main.cpp
@@ -14,13 +14,23 @@ int main() {
     // - Display actual JPEGs 
 
     InkyFrame inky;
-
     inky.init();
+
+    stdio_init_all();
+    sleep_ms(500);
+
+    printf("\n");
+    printf("\n");
+    printf("\n");
+
+    printf("initialising inky frame.. ");
+    printf("done!\n");
 
     // inky.led(InkyFrame::LED_ACTIVITY, 0);
 
     // inky.led(InkyFrame::LED_CONNECTION, 0);
 
+    printf("About to draw on the screen...\n");
 
     inky.set_pen(0);
     inky.clear();
@@ -29,9 +39,13 @@ int main() {
             uint colour = (y / 16) % 8;
             inky.set_pen(colour);
             inky.line(Point(0, y), Point(599, y));
+            printf("%d,", y);
     }
 
-    // inky.set_pen(1);
-    // inky.line(Point(0, 0), Point(599, 447));
+    printf("\n");
+    printf("Finished drawing image\n");
     inky.update();
+    printf("Updated screen\n");
+    printf("\n");
+    sleep_ms(500);
 }
