215e869efff5424a788236ab77cd5e2b7ca47725
Chris Jenkins <cdpjenkins@gmail.com>
2023-02-25 21:20:49 +0000

    Extract a few methods to make the main method a bit shorter and more manageable

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ using namespace pimoroni;
 
 InkyFrame inky;
 
+FATFS fs;
+
 JPEGDEC jpeg;
 struct {
   int x, y, w, h;
@@ -30,6 +32,10 @@ basic_string<char> read_text_file(const char *file_path);
 void http_get_to_file(const char *ip_addres, int port, const char *path, const char *file_path);
 
 void filesystem_list(const char *path);
+
+void mount_sd_card_filesystem();
+
+void connect_wifi();
 
 void *jpegdec_open_callback(const char *filename, int32_t *size) {
     FIL *fil = new FIL;
@@ -111,49 +117,18 @@ void draw_jpeg(std::string filename, int x, int y, int w, int h) {
 }
 
 int main() {
-    // TODOs
-    // - Do something with the LEDs
-    // - Use wifi to download pics
     inky.init();
 
     stdio_init_all();
     sleep_ms(1000);
 
-    for (int i = 0; i < 100; i++) {
-        cout << i << endl;
-        sleep_ms(10);
-    }
-    
-    cout << "Setting up WiFI" << endl;
-    cout << "Initialising CYW43... ";
-    if (cyw43_arch_init_with_country(CYW43_COUNTRY_UK)) {
-        cout << "Failed to init CYW43" << endl;
-        return 1;
-    }
-    cout << "Done!" << endl;
+    cout << "****************************" << endl;
+    cout << "Photo Gallery starting up..." << endl;
+    cout << "****************************" << endl << endl << endl;
 
-    cyw43_arch_enable_sta_mode();
+    connect_wifi();
 
-    cout << "Connecting to WiFi... ";
-    if (cyw43_arch_wifi_connect_blocking(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_MIXED_PSK)) {
-        cout << "Failed to connect to WiFi!" << endl;
-        return 2;
-    }
-    cout << "Connected!" << endl;
-
-    cout << "IP address: " << ip4addr_ntoa(netif_ip_addr4(netif_default)) << endl;
-    cout << "Netmask:    " << ip4addr_ntoa(netif_ip_netmask4(netif_default)) << endl;
-    cout << "Gateway:    " << ip4addr_ntoa(netif_ip_gw4(netif_default)) << endl;
-    cout << "Hostname:   " << netif_get_hostname(netif_default) << endl;
-
-    cout << "Mounting SD card filesystem... ";
-    FATFS fs;
-    FRESULT fr = f_mount(&fs, "", 1);
-    if (fr != FR_OK) {
-      cout << "Failed to mount SD card filesystem, error: " << fr << endl;
-      return 1;
-    }
-    cout << "Filesystem mounted!" << endl;
+    mount_sd_card_filesystem();
 
     // filesystem_list("/");
 
@@ -162,15 +137,15 @@ int main() {
     vector<string> files = split_by_newlines(read_text_file("/list.txt"));
 
     while (true) {
-        for (const auto &item: files) {
-            string jpeg_path = "/" + item;
-            string local_path = "/" + item;
+        for (const auto &jpeg_filename: files) {
+            string jpeg_path = "/" + jpeg_filename;
+            string local_path = "/" + jpeg_filename;
             http_get_to_file("192.168.1.51", 8000, jpeg_path.c_str(), local_path.c_str());
 
-            cout << "Drawing JPEG: " << item << "... " << endl;
+            cout << "Drawing JPEG: " << jpeg_filename << "... " << endl;
             inky.set_pen(0);
             inky.clear();
-            draw_jpeg(item, 0, 0, 600, 448);
+            draw_jpeg(jpeg_filename, 0, 0, 600, 448);
             cout << "Done drawing JPEG" << endl;
 
             cout << "Updating screen... ";
@@ -183,6 +158,40 @@ int main() {
 
     // Possibly isn't much point doing this, given that this line will never be reached...
     cyw43_arch_deinit();
+}
+
+void connect_wifi() {
+    cout << "Setting up WiFI" << endl;
+    cout << "Initialising CYW43... ";
+    if (cyw43_arch_init_with_country(CYW43_COUNTRY_UK)) {
+        cout << "Failed to init CYW43" << endl;
+//        return 1;
+    }
+    cout << "Done!" << endl;
+
+    cyw43_arch_enable_sta_mode();
+
+    cout << "Connecting to WiFi... ";
+    if (cyw43_arch_wifi_connect_blocking(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_MIXED_PSK)) {
+        cout << "Failed to connect to WiFi!" << endl;
+//        return 2;
+    }
+    cout << "Connected!" << endl;
+
+    cout << "IP address: " << ip4addr_ntoa(netif_ip_addr4(netif_default)) << endl;
+    cout << "Netmask:    " << ip4addr_ntoa(netif_ip_netmask4(netif_default)) << endl;
+    cout << "Gateway:    " << ip4addr_ntoa(netif_ip_gw4(netif_default)) << endl;
+    cout << "Hostname:   " << netif_get_hostname(netif_default) << endl;
+}
+
+void mount_sd_card_filesystem() {
+    cout << "Mounting SD card filesystem... ";
+    FRESULT fr = f_mount(&fs, "", 1);
+    if (fr != FR_OK) {
+      cout << "Failed to mount SD card filesystem, error: " << fr << endl;
+//      return 1;
+    }
+    cout << "Filesystem mounted!" << endl;
 }
 
 void filesystem_list(const char *path) {
